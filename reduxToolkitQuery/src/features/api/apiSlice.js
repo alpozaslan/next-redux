2f8a8e83d0bb077837b951efe31ad893e7d5593d
alpozaslan <alp.ozaslan07@gmail.com>
2022-12-21 01:37:51 +0300

    add tags to refresh cache

--- reduxToolkitQuery/src/features/api/apiSlice.js
+++ reduxToolkitQuery/src/features/api/apiSlice.js
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
+  tagTypes: ["Todos"],
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/todos",
+      providesTags: ["Todos"],
     }),
     addTodo: builder.mutation({
       query: (newTodo) => ({
@@ -13,6 +15,7 @@ export const apiSlice = createApi({
         method: "POST",
         body: newTodo,
       }),
+      invalidatesTags: ["Todos"],
     }),
     updateTodo: builder.mutation({
       query: (updatedTodo) => ({
@@ -20,6 +23,7 @@ export const apiSlice = createApi({
         method: "PATCH",
         body: updatedTodo,
       }),
+      invalidatesTags: ["Todos"],
     }),
     deleteTodo: builder.mutation({
       query: (todoId) => ({
@@ -27,6 +31,7 @@ export const apiSlice = createApi({
         method: "DELETE",
         body: todoId,
       }),
+      invalidatesTags: ["Todos"],
     }),
   }),
 });
